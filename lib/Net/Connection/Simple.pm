ed50309ff981c261082aecc716f015f1de685808
Wes Young <saxjazman@cpan.org>
2006-05-24 19:17:09 +0000

    Import of SAXJAZMAN/Net-Connection-Simple-1.02 from CPAN.
    gitpan-cpan-distribution: Net-Connection-Simple
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         SAXJAZMAN/net/Net-Connection-Simple-1.02.tar.gz
    gitpan-cpan-author:       SAXJAZMAN
    gitpan-cpan-maturity:     released

--- lib/Net/Connection/Simple.pm
+++ lib/Net/Connection/Simple.pm
@@ -1,80 +1,5 @@
 package Net::Connection::Simple;
 
-use 5.008007;
-use strict;
-use warnings;
-
-use Time::Timestamp;
-
-our $VERSION = '1.01';
-
-# CONSTRUCTOR
-sub new {
-	my ($class,%data) = @_;
-	my $self = {};
-	bless($self,$class);
-	$self->init(%data);
-	return $self;
-}
-
-#INIT
-sub init {
-	my ($self,%data) = @_;
-	$self->protocols(	$data{protocols});
-	$self->seenFirst(	$data{seenFirst});
-	$self->seenLast(	$data{seenLast});
-}
-
-# METHODS
-
-
-# ACCESSORS / MODIFIERS
-
-sub start {
-	my ($self,$v) = @_;
-	$self->{_start} = Time::Timestamp->new(ts => $v) if(defined($v));
-	return $self->{_start};
-}
-
-sub end {
-	my ($self,$v) = @_;
-	$self->{_end} = Time::Timestamp->new(ts => $v) if(defined($v));
-	return $self->{_end};
-}
-
-sub seenFirst {
-	my ($self,$v) = @_;
-	$self->{_seenFirst} = Time::Timestamp->new(ts => $v) if(defined($v));
-	return $self->{_seenFirst};
-}
-
-sub seenLast {
-	my ($self,$v) = @_;
-	$self->{_seenLast} = Time::Timestamp->new(ts => $v) if(defined($v));
-	return $self->{_seenLast};
-}
-#--------------
-# values:	Hashref or Net::Protocol::Simple
-# purpose:	Stores the different protocol layers for a connection
-sub protocols {
-	my ($self,$v) = @_;
-	if(defined($v)){
-		if(ref($v) eq 'HASHREF'){
-			foreach my $x (keys %$v){ $self->protocols($v);}
-		}
-		if(!(ref($v) =~ /Net::Protocol::Simple/)){
-			$self->errstr('Protocols requires a Net::Protocol::Simple Obj to be passed!');
-			return undef;
-		}
-		$self->{_protocols}->{$v->layer()} = $v;
-	}
-	return $self->{_protocols};
-}
-
-1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
-
 =head1 NAME
 
 Net::Connection::Simple - Perl extension handling simple connection info within an application
@@ -83,33 +8,103 @@ Net::Connection::Simple - Perl extension handling simple connection info within 
 
   use Net::Connection::Simple;
   my $c = Net::Connection::Simple->new(seenFirst => (time()-1800), seenLast => time());
+
   $c->protocols(Net::Protocol::Simple->new(protocol => tcp, layer => 4));
   $c->protocols(Net::Protocol::Simple->new(protocol => 'ip', layer => 3));
   $c->protocols(Net::Protocol::Simple->new(protocol => 'irc', layer => 7));
+
+  $c->protocols({
+  	1 => Net::Protocol::Simple->new(protocol => 6, layer => 4),
+  	2 => Net::Protocol::Simple->new(protocol => 'ip', layer => 3),
+  	3 => Net::Protocol::Simple->new(protocol => 'irc', layer => 7),
+  });
+
+  $c->protocols([
+  	Net::Protocol::Simple->new(protocol => 6, layer => 4),
+  	Net::Protocol::Simple->new(protocol => 'ip', layer => 3),
+  	Net::Protocol::Simple->new(protocol => 'irc', layer => 7),
+  ]);
 
 =head1 DESCRIPTION
 
   This module created to handle simple information about connections.
 
+=cut
+
+use 5.008007;
+use strict;
+use warnings;
+
+our $VERSION = '1.02';
+
 =head1 OBJECT METHODS
 
 =head2 new
 
-  Constructs the Connection object with the following properties:
-  	protocol: 	protocol (will accept 1,6 and 17 for ICMP,TCP,UDP) [string]
-  	layer: 		layer in the OSI model that the protocol resides [int]
-	start:		connection start [see Time::Timestamp]
-	end:		connection end [see Time::Timestamp]
-  	seenFirst: 	first instance of the connection [See Time::Timestamp]
-  	seenLast: 	last instance of the connection seen [See Time::Timestamp]
+Constructs the Connection object
 
-=head2 Time Properties
+Accepts:
 
-  [start|end|seenFirst|seenLast] will all return a Time::Timestamp object
+  	protocols => [ARRAY|HASHREF|Net::Protocol::Simple]
+
+=cut
+
+sub new {
+	my ($class,%data) = @_;
+	my $self = {};
+	bless($self,$class);
+	$self->protocols($data{protocols});
+	return $self;
+}
 
 =head2 protocols
 
-  Returns a HASHREF of the protocols composing the connection [See Net::Protocol::Simple]
+Returns a HASHREF of the protocols composing the connection [See Net::Protocol::Simple] keyed by layer
+
+Accepts:
+
+  	HASHREF:
+  		{ $key 	  => Net::Protocol::Simple(...),
+  		  $key++  => Net::Protocol::Simple(...),
+  		  $key++  => Net::Protocol::Simple(...),
+  		}
+
+  	ARRAY:
+  		[
+  		  Net::Protocol::Simple->new(...),
+  		  Net::Protocol::Simple->new(...),
+  		  Net::Protocol::Simple->new(...),
+  		]
+
+  	Net::Protocol::Simple:
+  		Net::Protocol::Simple->new(protocol => 6, layer => 4)
+
+=cut
+
+sub protocols {
+	my ($self,$v) = @_;
+	if(defined($v)){
+		for(ref($v)){
+			if(/^HASH$/){
+				foreach my $x (keys %$v){ $self->protocols($v->{$x});}
+				last;
+			}
+			if(/^ARRAY$/){
+				foreach my $x (@{$v}) { $self->protocols($x); }
+				last;
+			}
+			if(/^Net::Protocol::Simple$/){
+				$self->{_protocols}->{$v->layer()} = $v;
+				last;
+			}
+			die('Protocols requires a Net::Protocol::Simple Obj to be passed!');
+		}
+	}
+	return $self->{_protocols};
+}
+
+1;
+__END__
 
 =head1 SEE ALSO
 
